8adee85c534e360dc25e6cee3797f5618f3e8cda
Jerome Tan <jerome.tan@tuta.io>
2022-02-21 15:48:26 +0100

    generated tool model

---
+++ db/migrate/20220221144638_create_tools.rb
@@ -0,0 +1,16 @@
+class CreateTools < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tools do |t|
+      t.string :name
+      t.string :category
+      t.string :sub_category
+      t.text :description
+      t.boolean :available
+      t.integer :daily_price
+      t.integer :condition
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
