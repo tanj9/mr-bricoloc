346501764fa0b983797b99ed1606c7f02b39340c
Jerome Tan <jerome.tan@tuta.io>
2022-02-22 16:14:42 +0100

    Seed - fixed few bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "START SEEDING"
+puts 'START SEEDING'
 puts '0. cleaning database'
 User.destroy_all
 Tool.destroy_all
 Booking.destroy_all
 
-puts "1. SEED USERS"
+puts '1. SEED USERS'
 user = User.new(
   first_name: 'Adrien',
   last_name: 'Pointillart',
@@ -23,7 +23,7 @@ user = User.new(
   password: '000000'
 )
 file = URI.open('https://kitt.lewagon.com/placeholder/users/apointil')
-tool.photo.attach(io: file, filename: 'adrien.jpg', content_type: 'image/jpg')
+user.photo.attach(io: file, filename: 'adrien.jpg', content_type: 'image/jpg')
 user.save!
 puts "created user: #{user.first_name}"
 
@@ -37,7 +37,8 @@ user = User.new(
   password: '000000'
 )
 file = URI.open('https://kitt.lewagon.com/placeholder/users/rigardM')
-tool.photo.attach(io: file, filename: 'marion.jpg', content_type: 'image/jpg')
+user.photo.attach(io: file, filename: 'marion.jpg', content_type: 'image/jpg')
+user.save!
 puts "created user: #{user.first_name}"
 
 user = User.new(
@@ -50,7 +51,7 @@ user = User.new(
   password: '000000'
 )
 file = URI.open('https://kitt.lewagon.com/placeholder/users/FlorentGeron')
-tool.photo.attach(io: file, filename: 'florent.jpg', content_type: 'image/jpg')
+user.photo.attach(io: file, filename: 'florent.jpg', content_type: 'image/jpg')
 user.save!
 puts "created user: #{user.first_name}"
 
@@ -64,20 +65,20 @@ user = User.new(
   password: '000000'
 )
 file = URI.open('https://kitt.lewagon.com/placeholder/users/tanj9')
-tool.photo.attach(io: file, filename: 'jerome.jpg', content_type: 'image/jpg')
+user.photo.attach(io: file, filename: 'jerome.jpg', content_type: 'image/jpg')
 user.save!
 puts "created user: #{user.first_name}"
 
 puts "created #{User.count} users"
 
-puts "2. SEED TOOLS"
+puts '2. SEED TOOLS'
 user = User.find_by(first_name: 'Florent')
 tool = Tool.new(
-  name: "Wrench",
-  category: "works",
-  sub_category: "handtools",
+  name: 'Wrench',
+  category: 'works',
+  sub_category: 'handtools',
   daily_price: 5,
-  description: "Beautiful wrench. Silver color. Brand new. You can have it all weekdays.",
+  description: 'Beautiful wrench. Silver color. Brand new. You can have it all weekdays.',
   condition: 5,
   available: true
 )
@@ -88,11 +89,11 @@ tool.save!
 puts "created tool: #{tool.name}"
 
 tool = Tool.new(
-  name: "Ripper",
-  category: "works",
-  sub_category: "powertools",
+  name: 'Ripper',
+  category: 'works',
+  sub_category: 'powertools',
   daily_price: 15,
-  description: "Rips all you want",
+  description: 'Rips all you want',
   condition: 3,
   available: true
 )
@@ -103,11 +104,11 @@ tool.save!
 puts "created tool: #{tool.name}"
 
 tool = Tool.new(
-  name: "Two green screwdrivers",
-  category: "works",
-  sub_category: "handtools",
+  name: 'Two green screwdrivers',
+  category: 'works',
+  sub_category: 'handtools',
   daily_price: 1,
-  description: "Allow you to screw and unscrew.",
+  description: 'Allow you to screw and unscrew.',
   condition: 1,
   available: false
 )
@@ -118,11 +119,11 @@ tool.save!
 puts "created tool: #{tool.name}"
 
 tool = Tool.new(
-  name: "Hedge trimmer",
-  category: "garden",
-  sub_category: "motorized",
+  name: 'Hedge trimmer',
+  category: 'garden',
+  sub_category: 'motorized',
   daily_price: 15,
-  description: "Efficient and easy to use.",
+  description: 'Efficient and easy to use.',
   condition: 3,
   available: true
 )
@@ -134,11 +135,11 @@ puts "created tool: #{tool.name}"
 
 user = User.find_by(first_name: 'Marion')
 tool = Tool.new(
-  name: "Shovel",
-  category: "garden",
-  sub_category: "handtools",
+  name: 'Shovel',
+  category: 'garden',
+  sub_category: 'handtools',
   daily_price: 5,
-  description: "Oh so beautiful. Golden color.",
+  description: 'Oh so beautiful. Golden color.',
   condition: 4,
   available: true
 )
@@ -150,7 +151,7 @@ puts "created tool: #{tool.name}"
 
 puts "created #{Tool.count} tools"
 
-puts "3. SEED BOOKINGS"
+puts '3. SEED BOOKINGS'
 adrien = User.find_by(first_name: 'Adrien')
 marion = User.find_by(first_name: 'Marion')
 florent = User.find_by(first_name: 'Florent')
@@ -158,10 +159,10 @@ jerome = User.find_by(first_name: 'Jérôme')
 booking = Booking.new(
   user: marion,
   tool: florent.tools.first,
-  date_begin: "01/03/2022",
-  date_end: "02/03/2022",
+  date_begin: '01/03/2022',
+  date_end: '02/03/2022',
   total_price: 2 * florent.tools.first.daily_price,
-  status: "pending"
+  status: 'pending validation'
 )
 booking.save!
 puts "created booking: #{booking.user.first_name} / #{booking.tool.name}"
@@ -169,10 +170,10 @@ puts "created booking: #{booking.user.first_name} / #{booking.tool.name}"
 booking = Booking.new(
   user: jerome,
   tool: florent.tools.last,
-  date_begin: "02/03/2022",
-  date_end: "10/03/2022",
+  date_begin: '02/03/2022',
+  date_end: '10/03/2022',
   total_price: 9 * florent.tools.last.daily_price,
-  status: "pending"
+  status: 'pending validation'
 )
 booking.save!
 puts "created booking: #{booking.user.first_name} / #{booking.tool.name}"
@@ -180,10 +181,10 @@ puts "created booking: #{booking.user.first_name} / #{booking.tool.name}"
 booking = Booking.new(
   user: adrien,
   tool: marion.tools.last,
-  date_begin: "06/03/2022",
-  date_end: "10/03/2022",
+  date_begin: '06/03/2022',
+  date_end: '10/03/2022',
   total_price: 5 * marion.tools.last.daily_price,
-  status: "validated"
+  status: 'validated'
 )
 booking.save!
 puts "created booking: #{booking.user.first_name} / #{booking.tool.name}"
@@ -191,10 +192,10 @@ puts "created booking: #{booking.user.first_name} / #{booking.tool.name}"
 booking = Booking.new(
   user: adrien,
   tool: florent.tools.find_by(name: 'Ripper'),
-  date_begin: "04/03/2022",
-  date_end: "15/03/2022",
+  date_begin: '04/03/2022',
+  date_end: '15/03/2022',
   total_price: 12 * florent.tools.find_by(name: 'Ripper').daily_price,
-  status: "validated"
+  status: 'denied'
 )
 booking.save!
 puts "created booking: #{booking.user.first_name} / #{booking.tool.name}"
@@ -202,10 +203,10 @@ puts "created booking: #{booking.user.first_name} / #{booking.tool.name}"
 booking = Booking.new(
   user: jerome,
   tool: florent.tools.find_by(name: 'Two green screwdrivers'),
-  date_begin: "02/03/2022",
-  date_end: "03/03/2022",
+  date_begin: '02/03/2022',
+  date_end: '03/03/2022',
   total_price: 2 * florent.tools.find_by(name: 'Two green screwdrivers').daily_price,
-  status: "validated"
+  status: 'pending validation'
 )
 booking.save!
 puts "created booking: #{booking.user.first_name} / #{booking.tool.name}"
@@ -213,14 +214,14 @@ puts "created booking: #{booking.user.first_name} / #{booking.tool.name}"
 booking = Booking.new(
   user: marion,
   tool: florent.tools.find_by(name: 'Two green screwdrivers'),
-  date_begin: "05/03/2022",
-  date_end: "07/03/2022",
+  date_begin: '05/03/2022',
+  date_end: '07/03/2022',
   total_price: 3 * florent.tools.find_by(name: 'Two green screwdrivers').daily_price,
-  status: "validated"
+  status: 'validated'
 )
 booking.save!
 puts "created booking: #{booking.user.first_name} / #{booking.tool.name}"
 
 puts "created #{Booking.count} bookings"
 
-puts "DONE SEEDING"
+puts 'DONE SEEDING'
