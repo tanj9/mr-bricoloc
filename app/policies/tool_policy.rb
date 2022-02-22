6d7dcf60a80d4291dc5b961b243b44fee643c8ee
Jerome Tan <jerome.tan@tuta.io>
2022-02-22 10:47:35 +0100

    Pundit on tool - first commit

---
+++ app/policies/tool_policy.rb
@@ -0,0 +1,18 @@
+class ToolPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    # all users can add a new tool
+    true
+  end
+
+end
