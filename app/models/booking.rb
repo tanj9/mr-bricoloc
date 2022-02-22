c3897866d5b73bb525cbfac46be89206e146105f
Florent Geron <florent.geron@orange.fr>
2022-02-22 12:39:00 +0100

    ok

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,10 @@
 class Booking < ApplicationRecord
+STATUSES = ["pending validation", "validated", "denied", "canceled"]
+
   belongs_to :tool
   belongs_to :user
   validates :date_end, :date_begin, :tool, :user, presence: true
   validates_associated :tool
+
 
 end
