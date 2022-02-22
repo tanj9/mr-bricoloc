9b309672acc95f360ab6fadecf237a4a0850bc46
Florent Geron <florent.geron@orange.fr>
2022-02-22 10:50:34 +0100

    main methods

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,45 @@
+class BookingsController < ApplicationController
+
+def index
+  @bookings = Booking.all
+end
+
+def show
+  @booking = Booking.find(params[:booking_id])
+end
+
+def new
+  @tool = Tool.find(params[:id])
+  @booking = Booking.new
+end
+
+def create
+  @tool = Tool.find(params[:id])
+  @booking = Booking.new(booking_params)
+  @booking.user = current_user
+  @booking.tool = @tool
+  @booking.status = "pending validation"
+  if @booking.save
+    redirect_to bookings_path
+  else
+    render 'new'
+  end
+end
+
+def edit
+  @booking = Booking.find(params[:booking_id])
+end
+
+#def update
+#  @tool = Tool.find(params[:id])
+#  @booking = Booking.find(params[:booking_id])
+#  @booking.user = current_user
+#  @booking.tool = @tool
+#end
+
+private
+
+def booking_params
+params.require(:booking).permit(:date_begin, :date_end)
+end
+end
