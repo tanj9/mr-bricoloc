f71a7de35991811d2bd10b1257c7ca72a0bfe547
Florent Geron <florent.geron@orange.fr>
2022-02-23 13:40:21 +0100

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    @tools = policy_scope(Tool).order(created_at: :desc)
+    @tools = policy_scope(Tool).order(created_at: :desc).limit(6)
   end
 end
