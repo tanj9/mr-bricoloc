3df17639d97b0c29b22ad0e228c14b7d3e2b3142
Jerome Tan <jerome.tan@tuta.io>
2022-02-25 14:23:03 +0100

    implemented stimulus controller to switch tabs

--- app/javascript/controllers/switch_tab_controller.js
+++ app/javascript/controllers/switch_tab_controller.js
@@ -1,14 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
+import { end, variationPlacements } from "@popperjs/core"
 
 export default class extends Controller {
-  // static targets = [ "output" ]
-
-  connect() {
-
-  }
+  static targets = [ "tab", "content" ]
 
   switch(event) {
-    console.log(this);
-    console.log('coucou')
+    // console.log(event.currentTarget.dataset.tabName)
+    this.tabTargets.forEach((tab) => {
+      if (tab.dataset.tabName == event.currentTarget.dataset.tabName) {
+        tab.classList.add("active")
+      } else {
+        tab.classList.remove("active")
+      }
+    });
+    this.contentTargets.forEach((content) => {
+      if (content.dataset.tabName == event.currentTarget.dataset.tabName) {
+        content.classList.remove("d-none")
+      } else {
+        content.classList.add("d-none")
+      }
+    });
   }
 }
